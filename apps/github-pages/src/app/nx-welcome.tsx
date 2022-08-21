078799f8f1e85df70a521ca267c7b1ac53876271
Mark <madelk@gmail.com>
2022-08-21 14:49:26 -0500

    Start routes

--- apps/github-pages/src/app/nx-welcome.tsx
+++ apps/github-pages/src/app/nx-welcome.tsx
@@ -418,7 +418,7 @@ export function NxWelcome({ title }: { title: string }) {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome {title} 👋 gh token
+              Welcome {title} 👋
             </h1>
           </div>
 
