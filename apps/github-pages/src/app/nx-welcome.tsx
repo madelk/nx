34b2c1f81ae69de04f5082ecd0da5c0c3d62fd37
Mark <madelk@gmail.com>
2022-08-21 14:14:56 -0500

    Adding a user

--- apps/github-pages/src/app/nx-welcome.tsx
+++ apps/github-pages/src/app/nx-welcome.tsx
@@ -418,7 +418,7 @@ export function NxWelcome({ title }: { title: string }) {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome {title} 👋 butty
+              Welcome {title} 👋 user
             </h1>
           </div>
 
