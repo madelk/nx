0f137521a49150d457b1b9a26d284b3da7b79c44
Mark <madelk@gmail.com>
2022-08-21 17:47:51 -0500

    It's something

--- libs/components/roulette/src/lib/components-roulette.tsx
+++ libs/components/roulette/src/lib/components-roulette.tsx
@@ -6,40 +6,87 @@ import { useState } from 'react';
 export interface ComponentsRouletteProps {}
 
 const StyledComponentsRoulette = styled.div`
-  color: pink;
+  img {
+    display: block;
+    content: url('https://static-cdn.jtvnw.net/emoticons/v2/306872160/default/light/2.0');
+  }
+  text-align: center;
+  div {
+    margin: 0px auto;
+  }
+  canvas {
+    padding-left: 0;
+    padding-right: 0;
+    margin-left: auto;
+    margin-right: auto;
+    display: block;
+    border-radius: 50%;
+    background-size: contain;
+    background-image: url('https://pbs.twimg.com/profile_images/1410485845321207813/hKAF8XZ3_400x400.jpg');
+  }
 `;
-const StyledWheel = styled(Wheel)`
-  color: pink;
-`;
+const StyledWheel = styled(Wheel)``;
 const StyledButton = styled.button`
+  position: relative;
+  border-radius: 50%;
+  display: block;
+  margin: 0 auto;
+  background-color: red;
+
   color: pink;
+  width: 72px;
+  height: 72px;
+  background-image: url('https://static-cdn.jtvnw.net/badges/v1/dcc70f58-5334-4ec8-83d2-5a7ef30482ab/3');
+`;
+
+const StyledButtonArea = styled.div`
+  display: block;
+  position: relative;
+  text-align: center;
+`;
+const StyledWinner = styled.h2``;
+const StyledButtonText = styled.div`
+  text-transform: uppercase;
+  font-size: 12px;
 `;
 
 const data = [
-  { option: '0', style: { backgroundColor: 'green', textColor: 'black' } },
-  { option: '1', style: { backgroundColor: 'white' } },
-  { option: '2' },
+  { option: 'Dick' },
+  { option: 'Viewer Choice' },
+  { option: 'Chie' },
+  { option: 'Vector Man' },
+  { option: 'Sam choice', style: { backgroundColor: '#FF44AA' } },
 ];
 export function ComponentsRoulette(props: ComponentsRouletteProps) {
   const [mustSpin, setMustSpin] = useState(false);
   const [prizeNumber, setPrizeNumber] = useState(0);
+  const [winner, setWinner] = useState('Spin to find out');
 
   const handleSpinClick = () => {
     const newPrizeNumber = Math.floor(Math.random() * data.length);
     setPrizeNumber(newPrizeNumber);
+    setWinner('spinning...');
     setMustSpin(true);
+  };
+  const handleSpinStop = () => {
+    setMustSpin(false);
+    setWinner(data[prizeNumber].option);
   };
   return (
     <StyledComponentsRoulette>
+      <StyledWinner>Winner: {winner}</StyledWinner>
       <StyledWheel
+        backgroundColors={['red', 'black']}
+        textColors={['black', 'red']}
         mustStartSpinning={mustSpin}
         prizeNumber={prizeNumber}
         data={data}
-        onStopSpinning={() => {
-          setMustSpin(false);
-        }}
+        onStopSpinning={handleSpinStop}
       />
-      <StyledButton onClick={handleSpinClick}>SPIN</StyledButton>
+      <StyledButtonArea>
+        <StyledButton onClick={handleSpinClick} />
+        <StyledButtonText>SPIN</StyledButtonText>
+      </StyledButtonArea>
     </StyledComponentsRoulette>
   );
 }
