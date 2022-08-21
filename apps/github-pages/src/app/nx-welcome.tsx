c7143e17453980f0aeb1ed87fb91f2067e9d0e6e
Mark <madelk@gmail.com>
2022-08-21 14:26:58 -0500

    use env

--- apps/github-pages/src/app/nx-welcome.tsx
+++ apps/github-pages/src/app/nx-welcome.tsx
@@ -418,7 +418,7 @@ export function NxWelcome({ title }: { title: string }) {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome {title} 👋 origin
+              Welcome {title} 👋 gh token
             </h1>
           </div>
 
